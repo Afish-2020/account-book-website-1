abf4ee13803dde9dbb1851c3c7a509360e1c43ba
fishyabi <1027096592@qq.com>
2021-09-18 17:35:30 +0800

    deploy

---
+++ precache-manifest.dd53cc16690ef4fa76e41ce1d2a6bcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aa7784c0f4b9361a0b",
+    "url": "/account-book-website-1/css/app.87297afa.css"
+  },
+  {
+    "revision": "c58f0fa75a99cd069c99977353899f67",
+    "url": "/account-book-website-1/index.html"
+  },
+  {
+    "revision": "65aa7784c0f4b9361a0b",
+    "url": "/account-book-website-1/js/app.b88db9fb.js"
+  },
+  {
+    "revision": "d4a5a760a4a6abd9e79c",
+    "url": "/account-book-website-1/js/chunk-vendors.e8e961a1.js"
+  },
+  {
+    "revision": "a47193e4319456f6c5ae9f42b7306d13",
+    "url": "/account-book-website-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-book-website-1/robots.txt"
+  }
+]);
